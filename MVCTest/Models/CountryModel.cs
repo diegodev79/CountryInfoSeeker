c533574327b15ade4b35a553889aea478fbf9afa
diegodev79 <diegodev79@outlook.com>
2023-08-28 08:22:46 -0300

    SMall updates

---
+++ MVCTest/Models/CountryModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCTest.Models
+{
+    public class CountryModel
+    {
+        public string Name { get; set; }
+        public string Iso2Code { get; set; }
+        public string Iso3Code { get; set; }
+    }
+}
---
+++ MVCTest/Services/CountryService.cs
@@ -0,0 +1,34 @@
+﻿using MVCTest.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MVCTest.Services
+{
+    public class CountryService
+    {
+        private readonly HttpClient _httpClient;
+        private List<CountryModel> _countries;
+
+        public CountryService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task LoadCountriesAsync()
+        {
+            var response = await _httpClient.GetAsync("https://restcountries.com/v3/all");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                _countries = JsonSerializer.Deserialize<List<CountryModel>>(content);
+            }
+        }
+
+        public IReadOnlyList<CountryModel> GetCountries()
+        {
+            return _countries;
+        }
+    }
+}
